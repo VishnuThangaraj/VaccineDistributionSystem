69e0cbc11865a92aad9b15e22bd425739a3717f7
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-15 15:03:14 +0530

    initial commit

---
+++ src/main/java/com/vishnuthangaraj/VaccineDistributionSystem/VaccineDistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vishnuthangaraj.VaccineDistributionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineDistributionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineDistributionSystemApplication.class, args);
+	}
+
+}
